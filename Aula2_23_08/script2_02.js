70f29eb95e715c579668dd4d9bf25be631f448f9
Felipe <fehal@LAPTOP-E6FCL7BK>
2022-08-24 17:18:45 -0300

    Segunda aula

---
+++ Aula2_23_08/script2_02.js
@@ -0,0 +1,6 @@
+document.write("6 + 8 =<br> ");//Colocando entre aspas do document write ele somente mostra o texto.
+document.write(6 + 8);//Sem aspas ele realiza a operação descrita no código.
+/*
+Dentro das aspas pode ser inserida tags de HTML
+Ex. <br>, <p> e etc.
+*/
